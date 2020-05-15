6b18622915a959742f6daac2e0328ded59f78ba9
John Vandenberg <jayvdb@gmail.com>
2020-05-15 22:35:59 +0700

    User.is_authenticated is a property

--- restframework_stripe/permissions.py
+++ restframework_stripe/permissions.py
@@ -5,7 +5,7 @@ class OwnerOnlyPermission(BasePermission):
     """
     """
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated()
+        return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         return request.user and (request.user.is_staff or request.user == obj.owner)
