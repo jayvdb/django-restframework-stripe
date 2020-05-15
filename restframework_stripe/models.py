039744385192963e3b821208f27998c9f681e05c
John Vandenberg <jayvdb@gmail.com>
2020-05-15 23:38:46 +0700

    models: Use jsonfield
    This allows operation using sqlite3.

--- restframework_stripe/models.py
+++ restframework_stripe/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.postgres.fields import JSONField
+from jsonfield import JSONField
 from django.core.exceptions import ValidationError as DJValidationError
 
 import stripe
